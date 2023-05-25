8532d93922e201bda473ec77afe093cb85d28d7a
Daniel Alonso <nachodha@gmail.com>
2023-05-24 21:45:19 -0300

    nuevo commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+alert ("que te pasa gil")
+console.log("que te importa que me pasa")
+console.log('con comillas simples tb worka')
+let userName = 'Nachito pro'
+console.log (userName)
+console.log ('Mi nombre es '+ userName)
+alert ('puede que su codigo tenga')
+//sdfgdfgdf
